ecd3ae7116b53344fe1d23f6a426d9ea761f1635
Daniel Quinlan <daniel@chaosengine.net>
2026-09-09 05:51:50 +0000

    bump version to test CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "net.chaosengine.linkrouter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 101
-        versionName = "0.1.1"
+        versionCode = 102
+        versionName = "0.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
